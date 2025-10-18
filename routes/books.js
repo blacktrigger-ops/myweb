fb96dbcd55ba2829785e7b10be332cee1e8e95b0
blacktrigger-ops <blacktrigger-ops@users.noreply.github.com>
2025-10-18 23:14:08 +0600

    newly updated

--- routes/books.js
+++ routes/books.js
@@ -54,7 +54,7 @@ router.post('/', async(req, res) => {
     });
     saveCover(book, req.body.cover);
     try{
-         
+        const newBook = await book.save();
          res.redirect(`/books`)
 
     }catch{
